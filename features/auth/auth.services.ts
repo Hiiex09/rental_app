4824f0ad141e1a446aa23cc6e4b45d893fd59f88
barangandevon <barangandevon@gmail.com>
2026-04-15 16:37:03 +0800

    chore: added login system & and request limit if reach attemp matically locked

--- features/auth/auth.services.ts
+++ features/auth/auth.services.ts
@@ -32,3 +32,86 @@ export const signup = async (formData: any) => {
     });
     return newUser;
 };
+
+export const login = async (formData: any) => {
+    const { email, password } = formData;
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        throw new Error("Invalid credentials");
+    }
+
+    const now = new Date();
+
+    if (user.isLocked) {
+        if (user.lockUntil && user.lockUntil > now) {
+            const remainingMs = user.lockUntil.getTime() - now.getTime();
+            const remainingSeconds = Math.ceil(remainingMs / 1000);
+
+            throw {
+                message: "Account is locked",
+                status: 403,
+                remainingSeconds,
+            };
+        }
+
+        if (user.lockUntil && user.lockUntil <= now) {
+            await prisma.user.update({
+                where: { id: user.id },
+                data: {
+                    isLocked: false,
+                    failedAttempts: 0,
+                    lockUntil: null
+                }
+            });
+        }
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        const countAttempts = user.failedAttempts += 1;
+
+        if (countAttempts >= 3) {
+            const lockTime = new Date(now.getTime() + 3 * 60 * 1000);
+
+            await prisma.user.update({
+                where: { id: user.id },
+                data: {
+                    failedAttempts: countAttempts,
+                    isLocked: true,
+                    lockUntil: lockTime
+                }
+            })
+
+            throw new Error("Account locked due to too many failed attempts.");
+        }
+
+        await prisma.user.update({
+            where: { id: user.id },
+            data: {
+                failedAttempts: countAttempts,
+            },
+        });
+
+        throw new Error(`Invalid credentials. Attempt ${countAttempts}`);
+    }
+
+    await prisma.user.update({
+        where: { id: user.id },
+        data: {
+            failedAttempts: 0,
+            isLocked: false,
+            lockUntil: null,
+            lastLogin: now,
+        },
+    });
+
+    return {
+        id: user.id,
+        email: user.email,
+    };
+}
